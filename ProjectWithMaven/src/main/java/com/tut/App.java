c1946591150ec477fcbad23a3b001e649b6311b5
Akshay Rajendra Suryawanshi <51745558+akshayrajendra06@users.noreply.github.com>
2022-01-04 09:36:27 +0530

    Add files via upload

---
+++ ProjectWithMaven/src/main/java/com/tut/App.java
@@ -0,0 +1,25 @@
+package com.tut;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Project Started....!" );
+        
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        
+//        Configuration cfg = new Configuration();
+//        cfg.configure("hibernate.cfg.xml");
+//        SessionFactory factory = cfg.buildSessionFactory();
+        
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
+    }
+}
