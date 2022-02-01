9a71c76362fa53c2c49b351691ac5d3e5ccd9b35
Akshay Rajendra Suryawanshi <51745558+akshayrajendra06@users.noreply.github.com>
2022-02-01 09:23:05 +0530

    Add files via upload

---
+++ NODE JS Projexcct/aks1.js
@@ -0,0 +1,6 @@
+document.write('hello');
+document.write("newheloo");
+
+var x = "AKshay Cout";
+document.write(x);
+document.write(y);
---
+++ NODE JS Projexcct/aks3.js
@@ -0,0 +1,15 @@
+
+var kk =1;
+
+function sum(a,b){
+    return a+b;
+}
+// console.log(sum(2,5));
+        while(kk==1){
+            var a = prompt("Enter Your Number:");
+            var b = prompt("Enter Your Number :");
+            console.log(sum(a+b));
+            document.write(sum(a+b));
+            alert(a+b);
+            var kk = prompt("You Want to continew Please Enter 1");
+            }
